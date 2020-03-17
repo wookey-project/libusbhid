5613d8d06f40979c66901f5153d2f8df64982036
Philippe Thierry <phil@reseau-libre.net>
2020-03-17 10:48:01 +0100

    [update] remove useless calls

--- usbhid.c
+++ usbhid.c
@@ -57,6 +57,8 @@ static mbed_error_t usbhid_data_sent(uint32_t dev_id, uint32_t size, uint8_t ep_
     data_being_sent = false;
     dev_id = dev_id;
     size = size;
+
+    usb_backend_drv_ack(usbhid_ctx.iface.eps[1].ep_num, USB_BACKEND_DRV_EP_DIR_IN);
     return MBED_ERROR_NONE;
 }
 
@@ -124,7 +126,7 @@ mbed_error_t usbhid_declare(uint32_t usbxdci_handler,
     usbhid_ctx.iface.eps[1].dir         = USB_EP_DIR_IN;
     usbhid_ctx.iface.eps[1].attr        = USB_EP_ATTR_NO_SYNC;
     usbhid_ctx.iface.eps[1].usage       = USB_EP_USAGE_DATA;
-    usbhid_ctx.iface.eps[1].pkt_maxsize = 128; /* mpsize on EP2 */
+    usbhid_ctx.iface.eps[1].pkt_maxsize = 16; /* mpsize on EP1 */
     usbhid_ctx.iface.eps[1].ep_num      = 1; /* this may be updated by libctrl */
     usbhid_ctx.iface.eps[1].handler     = usbhid_data_sent;
 
@@ -166,10 +168,9 @@ mbed_error_t usbhid_send_report(uint8_t* report)
     data_being_sent = true;
     /* total size is report + report id (one byte) */
     log_printf("[USBHID] sending report on EP %d (len %d)\n", usbhid_ctx.iface.eps[1].ep_num, len+1);
-    usb_backend_drv_endpoint_disable(usbhid_ctx.iface.eps[1].ep_num, USB_EP_DIR_IN);
+    //usb_backend_drv_endpoint_disable(usbhid_ctx.iface.eps[1].ep_num, USB_EP_DIR_IN);
     usb_backend_drv_send_data(report, len+1, usbhid_ctx.iface.eps[1].ep_num);
 //    usb_backend_drv_endpoint_enable(usbhid_ctx.iface.eps[1].ep_num, USB_EP_DIR_IN);
-    //usb_backend_drv_ack(usbhid_ctx.iface.eps[1].ep_num, USB_BACKEND_DRV_EP_DIR_IN);
 err:
     return errcode;
 }
